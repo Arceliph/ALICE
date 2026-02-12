84bfcc2304fadc644fc35e479a655ec7e96dd00d
Arceliph <richard.jano37@outlook.com>
2026-02-11 18:16:53 -0600

    first commit

---
+++ ALICE.py
@@ -0,0 +1,96 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+import logging
+import os
+import api_methods
+
+def ALICE():
+    #Necessary setup for discord.py
+    load_dotenv()
+    token = os.getenv('DISCORD_TOKEN')
+
+    handler = logging.FileHandler(filename='./discord.log', encoding='utf-8', mode='w')
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.members = True
+
+    bot = commands.Bot(command_prefix='!', intents=intents)
+    role="Red"
+
+
+    @bot.event
+    async def on_ready():
+        print(f"We are ready to go in, {bot.user.name}")
+
+    @bot.event
+    async def on_member_join(member):
+        await member.send(f"{bot.user.name} acknowledged {member.name} joining!")    
+    
+
+    @bot.event
+    async def on_message(message):
+        #Don't reply to yourself
+        if message.author == bot.user:
+            return
+        
+        if "i hate alice" in message.content.lower():
+            await message.delete()
+            await message.channel.send(f"{message.author.mention}, that's mean :(")
+
+        if "mustikkapiirakka" in message.content.lower():
+            await message.channel.send(f"{message.author.mention} I need to eat Mustikkapiirakka with you {message.author.display_name} PLS PLS PLS PLS!!!1!!!!!!!!!!!")
+        
+        await bot.process_commands(message)
+
+    @bot.command()
+    async def sako(ctx):
+        await ctx.send(f"Sako makes some pretty neat rifles {ctx.author.display_name}!")
+
+    @bot.command()
+    async def red(ctx):
+        role_given = discord.utils.get(ctx.guild.roles, name=role)
+        if role_given:
+            await ctx.author.add_roles(role_given)
+            await ctx.send(f"{ctx.author.display_name} is now given {role_given}")
+        else:
+            await ctx.send(f"Error: Role does not exist")
+
+    @bot.command()
+    async def redRemove(ctx):
+        role_given = discord.utils.get(ctx.guild.roles, name=role)
+        if role_given:
+            await ctx.author.remove_roles(role_given)
+            await ctx.send(f"{ctx.author.display_name} is now losing {role_given}")
+        else:
+            await ctx.send(f"Error: Role does not exist")
+
+    @bot.command()
+    async def dm(ctx, *, msg):
+        await ctx.author.send(f"You said '{msg}'")
+
+    @bot.command()
+    async def reply(ctx):
+        await ctx.reply("Replying to message")
+
+    @bot.command()
+    async def badapple(ctx):
+        await ctx.send(file=discord.File("./BadApple.mp4", filename="BadApple.mp4"))#, embed=embed)
+
+    #Access Randomfox API to display an image of a random fox
+    @bot.command()
+    async def fox(ctx):
+        await api_methods.fox_api(ctx)
+        
+    #Access Safebooru's API and pull a random Furina image
+    @bot.command()
+    async def furina(ctx):
+        await api_methods.furina_api(ctx)
+
+
+    bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+
+
+if __name__ == "__main__":
+    ALICE()
---
+++ api_methods.py
@@ -0,0 +1,89 @@
+import discord
+import aiohttp
+from PIL import Image
+
+import io
+import re
+import xml.etree.ElementTree as ET
+import random
+
+DISCORD_SIZE_LIMIT_BYTES = 10_000_000
+
+def resize_image_bytes(img_buffer, target_filesize, tolerance=5):
+    """reads an image from a buffer and resizes in bytes instead of image dimensions"""
+    #TODO: This appears to resize to an acceptable size, but not the maximizing size that could be given
+    img = img_orig = Image.open(img_buffer).convert("RGB")
+
+    '''img = Image.new(
+        mode="RGB",
+        size=(int(img_width), int(img_height))
+    )'''
+    buffer = img_buffer
+    aspect = img.size[0] / img.size[1]
+
+    while True:
+        with io.BytesIO() as buffer:
+            img.save(buffer, format="JPEG")
+            data = buffer.getvalue()
+        filesize = len(data)    
+        size_deviation = filesize / target_filesize
+        print("size: {}; factor: {:.3f}".format(filesize, size_deviation))
+
+        if size_deviation <= (100 + tolerance) / 100:
+            # filesize fits
+            #print(data)
+            return io.BytesIO(data)
+        else:
+            # filesize not good enough => adapt width and height
+            # use sqrt of deviation since applied both in width and height
+            new_width = img.size[0] / size_deviation**0.5    
+            new_height = new_width / aspect
+            # resize from img_orig to not lose quality
+            img = img_orig.resize((int(new_width), int(new_height)))
+
+
+async def fox_api(ctx):
+    url = "https://randomfox.ca/floof/"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as fox_request:
+            if fox_request.status != 200:
+                return await ctx.send(f"Fox API failed with HTTP Status {fox_request.status}")
+
+            fox_json = await fox_request.json()
+            fox_image_loc = fox_json['image']
+            fox_image_number = re.findall(r'\d+', fox_image_loc)[0]
+
+            async with session.get(fox_image_loc) as fox_image_request:
+                fox_image_data = io.BytesIO(await fox_image_request.read())
+                await ctx.send(file=discord.File(fox_image_data, f"random_fox_{fox_image_number}.jpg"))
+
+
+async def furina_api(ctx, id_num=None):
+    #TODO: Add command options for searching artwork from Safebooru
+    random_limit = random.randint(0, 62)
+    url = f"https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=furina_(genshin_impact)&pid={random_limit}"
+    if id_num:
+        url = f"https://safebooru.org/index.php?page=dapi&s=post&q=index&id={id_num}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as furina_request:
+            if furina_request.status != 200:
+                return await ctx.send(f"Safebooru API failed with HTTP Status {furina_request.status}")
+        
+            furina_xml_data = await furina_request.read()
+            root = ET.fromstring(furina_xml_data)
+
+            random_index = random.randint(0, 99)
+            if id_num:
+                random_index = 0
+            root_post = root.findall("post")[random_index]
+            furina_img_loc = root_post.attrib.get("file_url")
+            #furina_img_height = (int) (root_post.attrib.get("height"))
+            #furina_img_width = (int) (root_post.attrib.get("width"))
+            furina_img_number = random_limit * 100 + random_index
+            
+            async with session.get(furina_img_loc) as furina_image_request:
+                furina_image_data = io.BytesIO(await furina_image_request.read())
+                if len(furina_image_data.getvalue()) >= DISCORD_SIZE_LIMIT_BYTES:
+                    print(f"Resizing Image {furina_img_number}")
+                    furina_image_data = resize_image_bytes(furina_image_data, DISCORD_SIZE_LIMIT_BYTES)
+                await ctx.send(file=discord.File(furina_image_data, f"random_furina_{furina_img_number}.jpg"))
